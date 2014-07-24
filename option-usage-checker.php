afa3148181a1257aa9ad8954c214dbd1efb84c90
Weston Ruter <weston@x-team.com>
2014-07-24 15:26:23 -0700

    Update comments

--- option-usage-checker.php
+++ option-usage-checker.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Option Usage Checker
- * Description: Make sure that options are less than 1MB when adding and updating them. This ensures compatibility with Memcached's 1MB limit. Also make sure that all options are explicitly added before they are updated. Define Option_Usage_Checker_OBJECT_CACHE_BUCKET_MAX_SIZE (number of bytes) in wp-config.php to override the default of 1MB. Option usage violations will throw exceptions if WP_DEBUG is turned on by default; otherwise PHP warnings will be issued.
+ * Description: Make sure that options are less than 1MB when adding and updating them. This is specifically intended for when using an external object cache, like Memcached, where 1MB is the max bucket size (e.g. this is the case on WordPress.com). This ensures compatibility with Memcached's 1MB limit. Also make sure that all options are explicitly added before they are updated. Define Option_Usage_Checker_OBJECT_CACHE_BUCKET_MAX_SIZE (number of bytes) in wp-config.php to override the default of 1MB. Option usage violations will throw exceptions if WP_DEBUG is turned on by default; otherwise PHP warnings will be issued.
  * Version: 0.1
  * Author: X-Team WP
  * Author URI: http://x-team.com/wordpress/
@@ -27,7 +27,7 @@ class Option_Usage_Checker_Plugin {
 
 	/**
 	 * Value goes through option_value_max_size filter when used. Set to
-	 * OPTION_USAGE_CHECKER_OBJECT_CACHE_BUCKET_MAX_SIZE or one 1MB. Value is in bytes.
+	 * OPTION_USAGE_CHECKER_OBJECT_CACHE_BUCKET_MAX_SIZE or 1MB. Value is in bytes.
 	 *
 	 * @var int
 	 */
